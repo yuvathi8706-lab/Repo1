7f38cb32e9c8754685ad7f8444554730baab746c
Yuvathi <yuvathi8706@gmail.com>
2026-07-15 09:30:23 +0000

    calculator

---
+++ p11.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        int a, b, res;
+        cin >> a >> b;
+        char op;
+        cin >> op;
+        switch(op){
+                case '+':
+                        res = a+b;
+                        break;
+                case '-':
+                        res = (a>b)?(a-b):(a+b);
+                        break;
+                default:
+                        res = 0;
+                        break;
+        }
+        cout << "Result: " << res;
+        return 0;
+}
